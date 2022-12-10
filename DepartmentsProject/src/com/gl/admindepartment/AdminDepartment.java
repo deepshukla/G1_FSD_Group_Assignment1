ab0e4342f35357954be489f1d68f84a1f6cf5ef9
deepshukla <shukla.deep86@gmail.com>
2022-12-10 21:19:04 +0530

    Add files via upload
    Add admin department file

---
+++ DepartmentsProject/src/com/gl/admindepartment/AdminDepartment.java
@@ -0,0 +1,21 @@
+package com.gl.admindepartment;
+
+public class AdminDepartment {
+	
+	//i) departmentName ii) getTodaysWork iii) getWorkDeadline
+	
+	String departmentName() {
+		return "Admin Department";
+	}
+	
+	String getTodaysWork() {
+		return "Complete your documents Submission";
+	}
+
+	
+	String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+
+
+}
